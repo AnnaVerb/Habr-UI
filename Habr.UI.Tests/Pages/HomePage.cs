01a01ded46964362f5a301e0e9766aa2b96b2675
Anna Verbitskaya <annystudy@gmail.com>
2020-08-21 19:11:21 +0300

    Created HomePage.cs

---
+++ Habr.UI.Tests/Pages/HomePage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Habr.UI.Tests.Pages
+{
+    public class HomePage
+    {
+        private IWebDriver Driver { get; set; }
+        public string Title
+        {
+            get
+            {
+                return Driver.Title;
+            }
+        }
+
+        public IWebElement ButtonLogo
+        {
+            get
+            {
+                return Driver.FindElement(By.ClassName("logo"));
+            }
+        }
+        public HomePage(IWebDriver driver)
+        {
+            Driver = driver;
+
+        }
+
+        public void GoHomePage()
+        {
+            
+
+            Driver.Navigate().GoToUrl("https://habr.com/ru/");
+            ButtonLogo.Click();
+
+        }
+
+
+
+    }
+}
--- Habr.UI.Tests/UnitTest1.cs
+++ Habr.UI.Tests/UnitTest1.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using Habr.UI.Tests.Pages;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Habr.UI.Tests
 {
@@ -11,8 +16,41 @@ namespace Habr.UI.Tests
         {
             Console.WriteLine("Test");
             Console.WriteLine("Test2");
-            Console.WriteLine("Test3");            
+            Console.WriteLine("Test3");
             Console.WriteLine("Test3.1");
         }
+
+        IWebDriver Driver { get; set; }
+
+        [TestInitialize] //запускается перед каждым тестом 
+        public void OpenBrowser()
+        {
+            Driver = GetChromeDriver();// метод ГетХромДр вернет нам созданный браузер в свойство
+            Driver.Manage().Window.Maximize();
+
+            HomePage page = new HomePage(Driver);
+            page.GoHomePage();
+        }
+
+
+        private IWebDriver GetChromeDriver()
+        {
+
+            string address = Assembly.GetExecutingAssembly().Location;
+
+            string outputDirectory = Path.GetDirectoryName(address);
+            return new ChromeDriver(outputDirectory);
+            //создание нового объекта типа ChromeDriver и вызов конструктора с параметром 
+        }
+
+
+        [TestMethod]
+        public void TestMethod_HomePageLogo()
+        {
+            var driver = new ChromeDriver();
+            
+
+        }
+
     }
 }
