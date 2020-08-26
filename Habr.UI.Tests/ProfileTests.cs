330ecc5df239940a9601668c8f164534996cbd52
Anna Verbitskaya <annystudy@gmail.com>
2020-08-26 14:10:09 +0300

    Creating Profile Tests

---
+++ Habr.UI.Tests/ProfileTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Habr.UI.Tests
+{
+    public class ProfileTests
+    {
+
+
+
+        [TestMethod]
+        public void Login_Success()
+        {
+
+            HomePage page = new HomePage(Driver);
+            page.GoHomePage();
+        }
+
+
+
+
+
+    }
